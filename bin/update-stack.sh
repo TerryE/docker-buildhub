0e67a6d4cc1817a14ae17684c8bcf511949680c7
terry <Terry@ellisons.org.uk>
2025-11-19 14:46:39 +0000

    Rebaseline upate stack check to use debian:13-slim only

--- bin/update-stack.sh
+++ bin/update-stack.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 # Loop around images and pulling each in turn.  If the ID has changed then set
 # BUILD=YES to flag that a stack rebuild is required.
 #
-IMAGES='mysql:8.0-debian alpine:3.19'
+IMAGES='debian:13-slim'
 declare BUILD id
 for i in $IMAGES; do
   id=$(docker inspect -f {{.ID}} $i)
